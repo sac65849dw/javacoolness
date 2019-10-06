263244ab66e90f5ceb905c2fa883b0513e3d7d55
terminator <sac65849@yahoo.com>
2019-10-05 22:06:50 -0400

    initialcommit

---
+++ src/app/App.java
@@ -0,0 +1,8 @@
+package app;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello Java");
+        System.out.println("this is a great program that I wrote");
+    }
+}
