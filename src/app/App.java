2bdc8bbe1aa36d94b325c48089c189eefab4b19f
terminator <sac65849@yahoo.com>
2019-10-05 22:28:16 -0400

    added file

--- src/app/App.java
+++ src/app/App.java
@@ -4,5 +4,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello Java");
         System.out.println("this is a great program that I wrote");
+        System.out.println("newl line");
     }
 }
